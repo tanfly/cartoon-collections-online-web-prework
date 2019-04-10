e0eb7f9b4df9fe0af18792eef95d80a0033a63c5
Stella McEwen <mcewen.stella@gmail.com>
2019-04-10 18:51:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types) == true 
-    return 
+    return cheese_types
+  else
+    return false
 end
