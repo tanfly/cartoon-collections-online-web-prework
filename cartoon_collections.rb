d59a62c6beca2fa8379f4a9b9e558c6bcc1affdf
Stella McEwen <mcewen.stella@gmail.com>
2019-04-10 18:23:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |elem|
+    
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
