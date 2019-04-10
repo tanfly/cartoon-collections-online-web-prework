1c9b82d4e5b3d2b33efcc86ad7ac8d1508b88424
Stella McEwen <mcewen.stella@gmail.com>
2019-04-10 18:53:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
 end
 end
